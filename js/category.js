e0ca8bd4a4a13add1aee0a60afe3397ba3ba938d
MonKeyXie <794566423@qq.com>
2018-08-19 06:13:56 +0800

    iScroll的简单使用

--- js/category.js
+++ js/category.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     /*区域滚动效果 */
-    /*条件：一个容器装着一个容器html结构 */
+    /*条件：一个容器装着一个容器html结构 && 自容器大于父容器*/
     /*找到大容器 */
     new IScroll(document.querySelector('.jd_cateLeft'),{
         scrollX:false,
